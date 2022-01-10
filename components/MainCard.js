f76a25818d9787088f72ac09c15f756bb0633ebf
blake-hodges <tbhodges@gmail.com>
2022-01-10 22:46:27 +0800

    fix main card temperature rounding error

--- components/MainCard.js
+++ components/MainCard.js
@@ -2,8 +2,8 @@ import Image from 'next/image'
 import styles from './MainCard.module.css'
 import { cToF } from '../services/converters.js'
 export const MainCard = (props) => {
-    let temperature = Math.round(props.weatherData.main.temp);
-    let feelsLike = Math.round(props.weatherData.main.feels_like);
+    let temperature = props.weatherData.main.temp;
+    let feelsLike = props.weatherData.main.feels_like;
     return (
         <div className={styles.wrapper}>
             <h1 className={styles.location}>{props.weatherData.name}, {props.weatherData.sys.country}</h1>
@@ -15,12 +15,12 @@ export const MainCard = (props) => {
                 alt="weather icon"
             />
             {(props.unitSystem == "metric")
-                ? <h1 className={styles.temperature}>{temperature}&deg;C</h1>
-                : <h1 className={styles.temperature}>{cToF(temperature)}&deg;F</h1>
+                ? <h1 className={styles.temperature}>{Math.round(temperature)}&deg;C</h1>
+                : <h1 className={styles.temperature}>{Math.round(cToF(temperature))}&deg;F</h1>
             }
             {(props.unitSystem == "metric")
-                ? <p>Feels like {feelsLike}&deg;C</p>
-                : <p>Feels like {cToF(feelsLike)}&deg;F</p>
+                ? <p>Feels like {Math.round(feelsLike)}&deg;C</p>
+                : <p>Feels like {Math.round(cToF(feelsLike))}&deg;F</p>
             }
 
         </div>
